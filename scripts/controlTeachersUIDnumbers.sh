063a41155a250e9115da9ce4ac06ca6fe432cb08
Reinhard Fink (APP) <reinhard.fink@tsn.at>
2016-10-10 11:33:56 +0200

    added script to list uidNumbers of Teachers

---
+++ scripts/controlTeachersUIDnumbers.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# source users and containers
+# following variables are sourced
+# ADMIN_ACCOUNT_NAME="cn=admin,dc=example,dc=com"
+# CONTAINER_NAME="ou=containerPupils,ou=users,dc=example,dc=com"
+# GROUP_NAME="cn=Default-Users,ou=groups,dc=example,dc=com"
+. ./secrets/controlTeachersGroupMembershipDefaultUsers-SECRET.sh
+
+#. ./controlContainerGroupMembershipLogic.sh
+
+echo "Please type LDAP - Password"
+read LDAPPASSWORD
+
+# search for posixAccount and just show attribute
+
+#ldapsearch -LLL -D "cn=admin,dc=app,dc=net" -W -h ldap01 -b "ou=containerTeachers,ou=users,dc=app,dc=net" dn uidNumber | sort 
+ldapsearch -LLL -D "$ADMIN_ACCOUNT_NAME" -h ldap01 -w $LDAPPASSWORD -b "$CONTAINER_NAME" -S uidNumber uidNumber dn 
+
